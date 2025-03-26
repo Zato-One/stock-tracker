88bd40cd2ad40fe05e08e7364fe5689d7c47e7cd
David Savič <davidsavic8@gmail.com>
2025-03-26 19:12:58 +0100

    Backend project init

---
+++ backend/stocktracker-core/src/main/java/cz/savic/stocktracker_core/StockTrackerCoreApplication.java
@@ -0,0 +1,24 @@
+package cz.savic.stocktracker_core;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class StockTrackerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackerCoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Application started: http://localhost:8080/");
+			// TODO - add swagger later
+//			System.out.println("Swagger UI available at: http://localhost:8080/swagger-ui.html");
+		};
+	}
+
+}
---
+++ backend/stocktracker-core/src/test/java/cz/savic/stocktracker_core/StockTrackerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.savic.stocktracker_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
